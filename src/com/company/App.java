64660f2503b9657c6036c41afa8471a9dffdf13c
richard.turner2@my.jcu.edu.au <Raztur1212>
2015-03-09 10:23:36 +1000

    Added useful comment to App

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -1,7 +1,10 @@
 package com.company;
 
 public class App {
-
+    /**
+     * Text viewer App - prints the contents of a string to the console
+     * Created by Richard Turner on 09/03/12
+     */
     public static void main(String[] args) {
 	String text = "This is a sample test\nOne Two Three\nHello world!";
         for (int i = 0; i<text.length(); ++i){
